d68f06279ff63ea4952abb4f238b4ffd76ec20b0
endrico221 <mediumroast221@gmail.com>
2023-02-13 22:55:10 +0200

    commit4:w

--- src/main/java/test/PocketMain.java
+++ src/main/java/test/PocketMain.java
@@ -9,13 +9,17 @@ public class PocketMain {
     }
 
     public int getMoney () {
+        //We provide an if statement, any money inserted below (positive) 10 return 0 value. This is done as specified
+        // in the provided Exercise part2.
         if (money <= 10) {
             return 0;
         }
+        //Here we have the main part which is going to return our money value from PocketMain.
         return money;
     }
 
     public void setMoney(int money) {
+
         if (money < 0) {
             System.out.println("You can't insert negative amount of money, Can you? Please try again!");
         } else if (money > 3000) {
